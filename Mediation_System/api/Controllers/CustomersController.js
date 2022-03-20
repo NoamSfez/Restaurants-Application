0dfad2c2721342eb669c63b253f111d77dc73744
NoamSfez <noam.sfez@gmail.com>
2022-03-20 09:36:03 +0200

    minor fix

--- Mediation_System/api/Controllers/CustomersController.js
+++ Mediation_System/api/Controllers/CustomersController.js
@@ -4,14 +4,14 @@ let instance = axios.create();
 
 export default class CustomersController {
   static async getAllCustomers(ctx, next) {
-    const rep = await instance.get("http://localhost:8080/api/employees/");
+    const rep = await instance.get("http://localhost:8080/api/customers/");
     ctx.response.body = { customers: rep.data };
   }
 
   static async getCustomerById(ctx, next) {
     const customer_id = ctx.params.customer_id;
     const rep = await instance.get(
-      `http://localhost:8080/api/employees/${customer_id}`
+      `http://localhost:8080/api/customer/${customer_id}`
     );
     ctx.response.body = { customer: rep.data };
   }
@@ -19,7 +19,7 @@ export default class CustomersController {
   static async createCustomer(ctx, next) {
     const body = ctx.request.body;
     const rep = await instance.post(
-      "http://localhost:8080/api/employees/",
+      "http://localhost:8080/api/customer/",
       body
     );
     ctx.response.body = { new_customer: rep.data };
@@ -29,7 +29,7 @@ export default class CustomersController {
     const customer_id = ctx.params.customer_id;
     const body = ctx.request.body;
     const rep = await instance.put(
-      `http://localhost:8080/api/employees/${customer_id}`,
+      `http://localhost:8080/api/customer/${customer_id}`,
       body
     );
     ctx.response.body = { updated_customer: rep.data };
@@ -38,14 +38,14 @@ export default class CustomersController {
   static async deleteCustomer(ctx, next) {
     const customer_id = ctx.params.customer_id;
     const rep = await instance.delete(
-      `http://localhost:8080/api/employees/${customer_id}`
+      `http://localhost:8080/api/customer/${customer_id}`
     );
     ctx.response.body = { response: rep.data };
   }
   // static async deleteAllCustomers(ctx, next) {
   //   const body = ctx.request.body;
   //   const rep = await instance.delete(
-  //     `http://localhost:8080/api/employees`,
+  //     `http://localhost:8080/api/customers`,
   //     body
   //   );
   //   ctx.response.body = { response: rep.data };
